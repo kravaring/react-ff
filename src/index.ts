8d7d04918d9e1fffadb89d1cf98effb438809c4b
yngvarr <yngvarr.bel@gmail.com>
2020-02-14 14:21:39 +0300

    Updated possible options

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
-import commander from 'commander';
+import commander, { Command } from 'commander';
 import { getFullPath } from './fileInteractions';
 
 const program = new commander.Command();
 
 program
-    .option('-j, --javascript', 'use javascript')
-    .option('-t, --test', 'add test file')
+    .option('-J, --javascript', 'use javascript')
+    .option('-t, --test [name]', 'add test file', 'test')
+    .option('-s, --style [option]', 'add style file', 'css')
     .version('0.0.1')
     .description('A CLI tool for creating feature folders in React')
     .command('create <Component> [destination]')
-    .action((component, destination, options) => {
-        const fullPath = getFullPath(component, __dirname, destination)
+    .action((component: string, destination: string, commandDetails: Command) => {
+        const fullPath = getFullPath(component, __dirname, destination);
         console.log(fullPath);
-        console.log(options.javascript);
-        console.log(options.test);
+        console.log(commandDetails.parent.javascript);
+        console.log(commandDetails.parent.test);
+        console.log(commandDetails.parent.style);
     });
 
 program.on('--help', function() {
